bb46a3dd7c2fa842933b28643b39caf6e636348d
brianmichaelu <brianmichaelu@gmail.com>
2026-06-04 10:31:30 +0300

    Adjust grid gap in properties display

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -46,7 +46,7 @@ export default function PropertiesPage() {
   </div>
 </div>
 
-        <div className="grid gap-7 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {properties.map((property) => (
             <PropertyCard key={property.id} property={property} />
           ))}
